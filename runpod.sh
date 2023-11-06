01c01ee344b97a9bb9b028498a8c924c914c3535
filpulsehex <150050467+filpulsehex@users.noreply.github.com>
2023-11-06 16:53:29 +0000

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -16,7 +16,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xbe5E24b167FEB66c4085f2a2afA1542F2AEfe91d/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
